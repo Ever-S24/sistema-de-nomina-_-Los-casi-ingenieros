37291460c01e885042bd110ab9a57b81063d7913
mhernandezm19-code <mhernandezm19@unicartagena.edu.co>
2026-04-06 18:43:42 -0500

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,6 +8,8 @@ public class Main {
 
         EmpleadoGeneral e1 = new EmpleadoAsalariado("Juan", 3000000, 6);
         EmpleadoGeneral e2 = new EmpleadoPorHoras("Ana", 20000, 45, 2, true);
+        EmpleadoGeneral e3 = new EmpleadoPorComision("Luis", 1500000, 25000000, 0.05);
+        EmpleadoGeneral e4 = new EmpleadoTemporal("Sofia", 1800000);
 
         System.out.println("Empleado: Juan");
         System.out.println("Tipo: Asalariado");
@@ -16,5 +18,13 @@ public class Main {
         System.out.println("\nEmpleado: Ana");
         System.out.println("Tipo: Por Horas");
         System.out.println("Salario neto: $" + e2.calcularSalarioNeto());
+
+        System.out.println("\nEmpleado: Luis");
+        System.out.println("Tipo: Por Comision");
+        System.out.println("Salario neto: $" + e3.calcularSalarioNeto());
+
+        System.out.println("\nEmpleado: Sofia");
+        System.out.println("Tipo: Temporal");
+        System.out.println("Salario neto: $" + e4.calcularSalarioNeto());
     }
 }
