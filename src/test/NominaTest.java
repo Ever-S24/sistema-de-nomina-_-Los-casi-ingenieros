09ba2735516fa75158aa4805f7bea7e45dc162a9
Everq <eversotomtz@gmail.com>
2026-04-08 13:17:51 -0500

    Tercera prueba unitaria - Las horas extra deben pagarse a 1.5x

--- src/test/NominaTest.java
+++ src/test/NominaTest.java
@@ -15,12 +15,23 @@ public class NominaTest {
         assertTrue(conBono.calcularSalarioNeto() > sinBono.calcularSalarioNeto());
 
     }
+    // Segunda prueba: imposibilidad de ventas negativas
+    @Test
+    public void testComision_VentasNegativas(){
+        assertThrows(IllegalArgumentException.class, ()-> {
+            new EmpleadoPorComision("Error", 1500000, -1000, 0.05);
+           });
+    }
 
-// Segunda prueba: imposibilidad de ventas negativas
-@Test
-public void testComision_VentasNegativas(){
-    assertThrows(IllegalArgumentException.class, ()-> {
-        new EmpleadoPorComision("Error", 1500000, -1000, 0.05);
-    });
-}
-}
+    //Tercera prueba: las horas extras deben pagarse a 1.5x
+    @Test
+    public void testPorHoras_HorasExtras() {
+        EmpleadoPorHoras emp = new EmpleadoPorHoras("Ana", 20000, 45, 0, false);
+        assertEquals(950000, emp.calcularSalario(), 0.01);
+    }
+
+
+
+
+
+}  
