f86fef3b21bff0730c70ed52f3b4b806acf81793
Everq <eversotomtz@gmail.com>
2026-04-08 19:53:48 -0500

    refactorización: main interactivo con Scanner

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,30 +1,21 @@
 package main;
-
-import model.*;
-
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("=== Sistema de Nomina ===\n");
+    public static void main(String [] arg) {
 
-        EmpleadoGeneral e1 = new EmpleadoAsalariado("Juan", 3000000, 6);
-        EmpleadoGeneral e2 = new EmpleadoPorHoras("Ana", 20000, 45, 2, true);
-        EmpleadoGeneral e3 = new EmpleadoPorComision("Luis", 1500000, 25000000, 0.05);
-        EmpleadoGeneral e4 = new EmpleadoTemporal("Sofia", 1800000);
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Empleado: Juan");
-        System.out.println("Tipo: Asalariado");
-        System.out.println("Salario neto: $" + e1.calcularSalarioNeto());
+        System.out.print(" == SISTEMA DE NOMINA ==");
+        System.out.println("Seleccione un tipo de empleado:");
+        System.out.println("1. Asalariado");
+        System.out.println("2. Por Horas");
+        System.out.println("3. Por Comision");
+        System.out.println("4. Temporal");
+        System.out.print("\nOpcion: ");
+        int opcion=scanner.nextInt();
 
-        System.out.println("\nEmpleado: Ana");
-        System.out.println("Tipo: Por Horas");
-        System.out.println("Salario neto: $" + e2.calcularSalarioNeto());
+        System.out.println("Tipo de empleado: " +opcion);
+        scanner.close();
 
-        System.out.println("\nEmpleado: Luis");
-        System.out.println("Tipo: Por Comision");
-        System.out.println("Salario neto: $" + e3.calcularSalarioNeto());
-
-        System.out.println("\nEmpleado: Sofia");
-        System.out.println("Tipo: Temporal");
-        System.out.println("Salario neto: $" + e4.calcularSalarioNeto());
     }
 }
