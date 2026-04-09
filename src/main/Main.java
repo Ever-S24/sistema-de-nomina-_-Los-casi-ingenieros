ae67ae2419d93bdeb5e584b8336e1e30f666dc7d
Everq <eversotomtz@gmail.com>
2026-04-08 21:42:47 -0500

    refactorización 3: ingreso de datos para empleado asalariado

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,12 @@
 package main;
+import model.*;
 import java.util.Scanner;
 public class Main {
     public static void main(String [] arg) {
 
         Scanner scanner = new Scanner(System.in);
 
+        //menú para tipos de empleados
         System.out.print(" == SISTEMA DE NOMINA ==");
         System.out.println("Seleccione un tipo de empleado:");
         System.out.println("1. Asalariado");
@@ -14,13 +16,27 @@ public class Main {
         System.out.print("\nOpcion: ");
         int opcion=scanner.nextInt();
 
+        //mostrar el tipo de empleado
         System.out.println("Tipo de empleado: " +opcion);
         scanner.nextLine();
 
+        //pedir el nombre del empleado
         System.out.print("Nombre del empleado: ");
         String nombre = scanner.nextLine();
 
-        System.out.println("Tipo: " + opcion + " | Nombre: " + nombre);
+        EmpleadoGeneral empleado = null ;
+
+        // si el empleado es asalariado se deben pedir el salario base y los años de antiguedad
+        if (opcion==1){
+            System.out.print("Salario base: ");
+            double salarioBase=scanner.nextDouble();
+            System.out.print("Años en la empresa: ");
+            int años=scanner.nextInt();
+            empleado=new EmpleadoAsalariado(nombre, salarioBase, años);
+
+        }
+
+       
 
 
 
