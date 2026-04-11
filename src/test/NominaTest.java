541a1daad7f2f8ec7a6ccb4102e73265a487cf07
Everq <eversotomtz@gmail.com>
2026-04-11 11:26:01 -0500

    Pruebas unitarias - Empleado asalariado

--- src/test/NominaTest.java
+++ src/test/NominaTest.java
@@ -11,21 +11,22 @@ public class NominaTest {
 
 //                                 EMPLEADO ASALARIADO
 
-//    Prueba 1: un empleado con más de 5 años en la empresa debe recibir un bono de antiguedad del 10%.
-//    Vamos a compararlo con uno con menos años en la empresa para verificar la diferencia.
-
+//      Prueba 1: un empleado con más de 5 años en la empresa debe recibir un bono de antiguedad del 10%.
+//                Vamos a compararlo con uno con menos años en la empresa para verificar la diferencia.
 @Test
-public void testAsalariado_ConBonoAntiguedad() {
+public void testAsalariado_ConBonodeantiguedad() {
     EmpleadoAsalariado conBono =new EmpleadoAsalariado("juan v", 2000000, 6);
     EmpleadoAsalariado sinBono =new EmpleadoAsalariado("luis p", 2000000, 3);
-    assertTrue(conBono.calcularSalarioNeto() > sinBono.calcularSalarioNeto());
-}
+    assertTrue(conBono.calcularSalarioNeto() > sinBono.calcularSalarioNeto());}
 
 
-
-
-
-
+//      Prueba 2: un empleado con exactamente 5 años no debe recibir el bono de antiguedad. 
+//                este bono solo aplica si lleva más de 5 años, no 5 exactos.
+@Test
+    public void testAsalariado_SinBonodeantiguedad() {
+        EmpleadoAsalariado cincoAños = new EmpleadoAsalariado("Maria s", 2000000, 5);
+        EmpleadoAsalariado seisAños  = new EmpleadoAsalariado("Juan v",  2000000, 6);
+        assertTrue(cincoAños.calcularSalarioNeto() < seisAños.calcularSalarioNeto());}
 
 
 
