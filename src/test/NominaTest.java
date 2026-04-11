9839c1ccba013a60824bae8d5608c513fa09a499
Everq <eversotomtz@gmail.com>
2026-04-11 13:42:15 -0500

    Pruebas unitarias - Empleado por horas

--- src/test/NominaTest.java
+++ src/test/NominaTest.java
@@ -15,18 +15,60 @@ public class NominaTest {
 //                Vamos a compararlo con uno con menos años en la empresa para verificar la diferencia.
 @Test
 public void testAsalariado_ConBonodeantiguedad() {
-    EmpleadoAsalariado conBono =new EmpleadoAsalariado("juan v", 2000000, 6);
-    EmpleadoAsalariado sinBono =new EmpleadoAsalariado("luis p", 2000000, 3);
+    EmpleadoAsalariado conBono =new EmpleadoAsalariado("juan v", 3000000, 6);
+    EmpleadoAsalariado sinBono =new EmpleadoAsalariado("luis p", 3000000, 3);
     assertTrue(conBono.calcularSalarioNeto() > sinBono.calcularSalarioNeto());}
 
 
 //      Prueba 2: un empleado con exactamente 5 años no debe recibir el bono de antiguedad. 
 //                este bono solo aplica si lleva más de 5 años, no 5 exactos.
 @Test
-    public void testAsalariado_SinBonodeantiguedad() {
-        EmpleadoAsalariado cincoAños = new EmpleadoAsalariado("Maria s", 2000000, 5);
-        EmpleadoAsalariado seisAños  = new EmpleadoAsalariado("Juan v",  2000000, 6);
-        assertTrue(cincoAños.calcularSalarioNeto() < seisAños.calcularSalarioNeto());}
+public void testAsalariado_SinBonodeantiguedad() {
+    EmpleadoAsalariado cincoAños = new EmpleadoAsalariado("Maria s", 3000000, 5);
+    EmpleadoAsalariado seisAños  = new EmpleadoAsalariado("Juan v",  3000000, 6);
+    assertTrue(cincoAños.calcularSalarioNeto() < seisAños.calcularSalarioNeto());}
+
+
+
+
+//                                  EMPLEADO POR HORAS
+
+//      Pruba 3: lsa horas extra (+40hrs) se pagan a 1.5x.
+//               si se trabajan 45 horas, equivale a 40hrs normales + 5hrs extra.
+@Test
+public void testPorHoras_HorasExtra() {
+    EmpleadoPorHoras emp=new EmpleadoPorHoras("Ana n", 20000, 45, 0, false);
+    assertEquals(950000, emp.calcularSalario(), 0.01);}
+
+
+//      Prueba 4: con solo 40hrs no debe haber pago de horas extra.
+//      
+@Test
+public void testPorHoras_SinHorasExtra() {
+    EmpleadoPorHoras emp = new EmpleadoPorHoras("Carlos p", 20000, 40, 0, false);
+    assertEquals(800000, emp.calcularSalario(), 0.01);}
+
+
+//      Prueba 5: si se ingresan horas negativas, el sistema debe lanzar una excepción.
+//
+@Test
+public void testPorHoras_ExcepcionHorasNegativas() {
+    assertThrows(IllegalArgumentException.class,()->{
+    new EmpleadoPorHoras("Error", 20000, -5, 0, false);});}
+
+
+//      Prueba 6: el fondo de ahorro es opcional y solo debe estar disponible para empleados con más de un año de antiguedad.
+//                si se acepta el fondo de ahorro, el salario neto debe ser menor.
+@Test
+public void testPorHoras_DescuentoFondodeAhorro() {
+    EmpleadoPorHoras conFondo = new EmpleadoPorHoras("Lisa", 20000, 40, 2, true);
+    EmpleadoPorHoras sinFondo = new EmpleadoPorHoras("Mike", 20000, 40, 2, false);
+    assertTrue(conFondo.calcularSalarioNeto() < sinFondo.calcularSalarioNeto());}
+
+
+//      
+
+
 
 
 
