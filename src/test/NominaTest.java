7f956f3724a73a52576a151dd7427bcdd1e4a1e8
Everq <eversotomtz@gmail.com>
2026-04-11 11:24:53 -0500

    Pruebas unitarias - Empleado asalariado

--- src/test/NominaTest.java
+++ src/test/NominaTest.java
@@ -5,7 +5,27 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 // Pruebas unitarias del sistema de nomina
+// Se verificarán las reglas de negocio para cada tipo de emleado
+
 public class NominaTest {
+
+//                                 EMPLEADO ASALARIADO
+
+//    Prueba 1: un empleado con más de 5 años en la empresa debe recibir un bono de antiguedad del 10%.
+//    Vamos a compararlo con uno con menos años en la empresa para verificar la diferencia.
+
+@Test
+public void testAsalariado_ConBonoAntiguedad() {
+    EmpleadoAsalariado conBono =new EmpleadoAsalariado("juan v", 2000000, 6);
+    EmpleadoAsalariado sinBono =new EmpleadoAsalariado("luis p", 2000000, 3);
+    assertTrue(conBono.calcularSalarioNeto() > sinBono.calcularSalarioNeto());
+}
+
+
+
+
+
+
 
 
 
